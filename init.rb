e0cfae3453e51d20182442371cef44d08d654311
Graeme Mathieson <mathie@woss.name>
2006-05-09 11:40:59 +0000

    * Form builders are now included into ApplicationHelper automatically. * Add some sparse documentation. * Remove unused files.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/form_builders@11 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
-# Include hook code here
+# Automatically include the new form helpers in ApplicationHelper
+[Rubaidh::TabularForm].each do |form_builder|
+  ApplicationHelper.send :include, form_builder
+end
