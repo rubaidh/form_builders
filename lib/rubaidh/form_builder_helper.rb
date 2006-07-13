0fbfb544d1a856390a9c35dfb44fe47db77ab1e4
Graeme Mathieson <mathie@woss.name>
2006-07-13 10:53:04 +0000

    * Pop the options, since we're appending them back on the end again   afterwards.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/form_builders@31 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- lib/rubaidh/form_builder_helper.rb
+++ lib/rubaidh/form_builder_helper.rb
@@ -2,7 +2,7 @@
 # form builder.
 def custom_form_for(builder, fields_pre, fields_post, form_tag, object_name, *args, &proc)
   raise ArgumentError, "Missing block" unless block_given?
-  options = args.last.is_a?(Hash) ? args.last : {}
+  options = args.last.is_a?(Hash) ? args.pop : {}
   concat(form_tag, proc.binding)
   concat(fields_pre, proc.binding) 
   fields_for(object_name, *(args << options.merge(:builder => builder)), &proc)
