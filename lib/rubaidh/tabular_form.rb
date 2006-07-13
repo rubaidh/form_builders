dca040f9f791c5d9f08ddfdf4af87070cb70245f
Graeme Mathieson <mathie@woss.name>
2006-07-13 10:13:27 +0000

    * Tabular form builder is refactored a little and now copes with   rendering check boxes, radio buttons & file_column fields.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/form_builders@29 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- lib/rubaidh/tabular_form.rb
+++ lib/rubaidh/tabular_form.rb
@@ -6,19 +6,57 @@ module Rubaidh
     class TabularFormBuilder < ActionView::Helpers::FormBuilder 
       (field_helpers - %w(check_box radio_button)).each do |selector| 
         src = <<-END_SRC 
-          def #{selector}(field, options = {}) 
-            @template.content_tag("tr", 
-              @template.content_tag("td", 
-                @template.content_tag("label", field.to_s.humanize + ":", {:for => @object_name + "_" + field})
-              ) + 
-              @template.content_tag("td", super)) 
+          def #{selector}(field, options = {})
+            label_text = options[:label] || options["label"] || field.to_s.humanize
+            generic_field(field, super, label_text)
           end 
         END_SRC
         class_eval src, __FILE__, __LINE__ 
       end
-  
+      
+      %w(check_box radio_button).each do |selector|
+        src = <<-END_SRC
+          def #{selector}(field, options = {})
+            label_text = options[:label] || options["label"] || field.to_s.humanize
+            generic_field(field, super, label_text, :label => :after)
+          end
+        END_SRC
+        class_eval src, __FILE__, __LINE__ 
+      end
+      
       def submit(text, options = {})
-        @template.content_tag("tr", @template.content_tag("td", @template.submit_tag(text, options), :colspan => 2, :style => "text-align: right;"))
+        generic_field(nil, @template.submit_tag(text, options))
+      end
+      
+      def file_column_field(field, options = {})
+        label_text = options[:label] || options["label"] || field.to_s.humanize
+        generic_field(field, @template.file_column_field(@object_name, field, options), label_text)
+      end
+      
+      protected
+      def generic_field(fieldname, field, label_text = nil, options = {})
+        unless label_text.blank?
+          if options[:label] == :after
+            tr(td('') + td(field + label(label_text, "#{@object_name}_#{fieldname}", true)))
+          else
+            tr(
+              td(label(label_text, "#{@object_name}_#{fieldname}")) +
+              td(field)
+            ) 
+          end
+        else # No label
+          tr(td(field, :colspan => 2, :style => "text-align: right;"))
+        end
+      end
+      
+      def tr content, options = {}
+        @template.content_tag 'tr', content, options
+      end
+      def td content, options = {}
+        @template.content_tag 'td', content, options
+      end
+      def label text, for_field, after = false
+        @template.content_tag 'label', "#{text}#{after ? '' : ':'}", :for => for_field
       end
     end 
 
