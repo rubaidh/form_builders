e402d291dfe3453ab188ba9671004fb7b502b763
Graeme Mathieson <mathie@woss.name>
2007-07-06 06:06:39 +0000

    Oops, that should be pushing into ActionView::Base, not ApplicationHelper.  What a rookie mistake\!
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/form_builders@67 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 # Automatically include the new form helpers in ApplicationHelper
 [Rubaidh::TabularForm].each do |form_builder|
-  ApplicationHelper.send :include, form_builder
+  ActionView::Base.send :include, form_builder
 end
